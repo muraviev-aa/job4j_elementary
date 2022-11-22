628c7bfd02b353598711c28dbda027a34451ece6
Aleksei Muraviev <muraviev-aa@yandex.ru>
2022-11-22 14:56:30 +0300

    6. Checkstyle [#4123] Ошибок не было

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Aleksei Muraviev");
+        System.out.println("19.08.1974");
+    }
+}
