64c5e5adf559183a4cc0b7ffcc683ca01e97ac15
Aleksei Muraviev <muraviev-aa@yandex.ru>
2022-11-25 10:20:09 +0300

    4.1.2. Глупый бот [#31323]

---
+++ src/test/java/ru/job4j/condition/DummyBotTest.java
@@ -0,0 +1,31 @@
+package ru.job4j.condition;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+
+public class DummyBotTest {
+
+    @Test
+    public void whenGreetBot() {
+        String in = "Hi, Bot.";
+        String result = DummyBot.answer(in);
+        String expected = "Hi, SmartAss.";
+        Assertions.assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    public void whenByeBot() {
+        String in = "Bye.";
+        String result = DummyBot.answer(in);
+        String expected = "See you later.";
+        Assertions.assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    public void whenUnknownBot() {
+        String in = "Can you add two plus two?";
+        String result = DummyBot.answer(in);
+        String expected = "I don't know. Please, ask another question.";
+        Assertions.assertThat(result).isEqualTo(expected);
+    }
+}
