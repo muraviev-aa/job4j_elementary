a9fac01a46ae13d149195a2e28e63ee03800df69
Aleksei Muraviev <muraviev-aa@yandex.ru>
2022-11-29 11:54:44 +0300

    6.0.3. Заполнение массива. [#226300]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -6,8 +6,17 @@ public class ArrayDefinition {
         String[] surnames = new String[100500];
         float[] prices = new float[40];
 
+        String[] names = new String[4];
+        names[0] = "Lusja Zvetkova";
+        names[1] = "Teodor Backer";
+        names[2] = "Mark Conor";
+
         System.out.println("Размер ages равен: " + ages.length);
         System.out.println("Размер surnames равен: " + surnames.length);
         System.out.println("Размер prices равен: " + prices.length);
+        System.out.println("----");
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
     }
 }
