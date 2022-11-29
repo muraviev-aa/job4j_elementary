4403e17994105af17a14ca533949a12d38105f1a
Aleksei Muraviev <muraviev-aa@yandex.ru>
2022-11-29 11:41:29 +0300

    6.0.2. Размер массива [#333582]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -5,5 +5,9 @@ public class ArrayDefinition {
         short[] ages = new short[10];
         String[] surnames = new String[100500];
         float[] prices = new float[40];
+
+        System.out.println("Размер ages равен: " + ages.length);
+        System.out.println("Размер surnames равен: " + surnames.length);
+        System.out.println("Размер prices равен: " + prices.length);
     }
 }
