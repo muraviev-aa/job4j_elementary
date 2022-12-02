8da7ec1c34a078aed5afd24f6f4e90a70bb53db2
Aleksei Muraviev <muraviev-aa@yandex.ru>
2022-12-02 10:12:58 +0300

    6.4.0. Слово начинается с ... [#41585 #366687]

--- src/main/java/ru/job4j/array/ArrayChar.java
+++ src/main/java/ru/job4j/array/ArrayChar.java
@@ -2,11 +2,10 @@ package ru.job4j.array;
 
 public class ArrayChar {
     public static boolean startsWith(char[] word, char[] pref) {
-        boolean result = false;
-        for (int index = 0; index < word.length; index++) {
-            if (word[index] == pref[index]) {
-                result = true;
-                break;
+        boolean result = true;
+        for (int index = 0; index < pref.length - 1; index++) {
+            if (word[index] != pref[index]) {
+                result = false;
             }
         }
         return result;
