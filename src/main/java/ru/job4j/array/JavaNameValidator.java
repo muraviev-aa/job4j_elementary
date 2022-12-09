078021d44076af9251dda7ff35ade96949f7e2ca
Aleksei Muraviev <muraviev-aa@yandex.ru>
2022-12-09 10:23:06 +0300

    Чтение кода и базовые ошибки

--- src/main/java/ru/job4j/array/JavaNameValidator.java
+++ src/main/java/ru/job4j/array/JavaNameValidator.java
@@ -2,13 +2,13 @@ package ru.job4j.array;
 
 public class JavaNameValidator {
     public static boolean isNameValid(String name) {
-        boolean rsl =  !name.isEmpty() && isRenamedNotUpperLatinLetter(name.charAt(0))
+        boolean rsl =  !name.isEmpty() && isNotUpperLatinLetter(name.charAt(0))
                 && !Character.isDigit(name.charAt(0));
 
         for (int i = 1; i < name.length(); i++) {
             int code = name.codePointAt(i);
-            if (isRenamedNotSpecialSymbol(code) && !Character.isDigit(code)
-            && isRenamedNotUpperLatinLetter(code) && isRenamedNotLowerLatinLetter(code)) {
+            if (isNotSpecialSymbol(code) && !Character.isDigit(code)
+            && isNotUpperLatinLetter(code) && isNotLowerLatinLetter(code)) {
                 rsl = false;
                 break;
             }
@@ -17,15 +17,15 @@ public class JavaNameValidator {
         return rsl;
     }
 
-    public static boolean isRenamedNotSpecialSymbol(int code) {
+    public static boolean isNotSpecialSymbol(int code) {
         return code != 36 && code != 95;
     }
 
-    public static boolean isRenamedNotUpperLatinLetter(int code) {
+    public static boolean isNotUpperLatinLetter(int code) {
         return code < 65 || code > 90;
     }
 
-    public static boolean isRenamedNotLowerLatinLetter(int code) {
+    public static boolean isNotLowerLatinLetter(int code) {
         return code < 97 || code > 122;
     }
 }
