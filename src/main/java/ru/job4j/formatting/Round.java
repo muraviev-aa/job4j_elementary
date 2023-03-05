4a587e17c76cc73711c0ef6935007274788fed22
Aleksei Muraviev <muraviev-aa@yandex.ru>
2023-03-05 09:41:57 +0300

    Added Round.

---
+++ src/main/java/ru/job4j/formatting/Round.java
@@ -0,0 +1,32 @@
+package ru.job4j.formatting;
+
+public class Round {
+    /** Округляем число в большую сторону,
+     * если его дробная часть превышает этот порог */
+    public static final double THRESHOLD = 0.54;
+
+    static long round(double d) {
+        long di = (long) Math.floor(d);
+        if ((d - di) > THRESHOLD) {
+            return di + 1;
+        } else {
+            return di;
+        }
+    }
+
+    public static void main(String[] args) {
+        double a = 3.999999;
+        /** Округление до числа с плавающей точкой до ближайшего
+         * целого с помощью метода round(a) */
+        System.out.println(round(a));
+
+        int b = (int) a;
+        /** Округление без использования метода round(a) */
+        System.out.println(b);
+
+        double c = 3.55;
+        System.out.println(round(c));
+        double d = 3.54;
+        System.out.println(round(d));
+    }
+}
