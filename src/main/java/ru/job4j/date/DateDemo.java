9e7854dd3b67327e988aa2b8716bfbdfd038596e
Aleksei Muraviev <muraviev-aa@yandex.ru>
2023-03-08 10:06:53 +0300

    Added DateDemo.

---
+++ src/main/java/ru/job4j/date/DateDemo.java
@@ -0,0 +1,17 @@
+package ru.job4j.date;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class DateDemo {
+    public static void main(String[] args) {
+        Date dNow = new Date();
+        System.out.println("It is now " + dNow.toString());
+        /** Используем SimpleDateFormat для печати даты */
+        /** EEEE - день недели; yyyy - год; MM - месяц года; dd - день года;
+         * hh - час; mm - минута; ss - секунда; a - половина суток (AM или PM)
+         * zzz - временная зона */
+        SimpleDateFormat formatter = new SimpleDateFormat("EEEE yyyy.MM.dd 'at' hh:mm:ss a zzz");
+        System.out.println("It is " + formatter.format(dNow));
+    }
+}
