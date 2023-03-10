b2bc2c001e142a41c9fa08fa340e102fdb6bee9b
Aleksei Muraviev <muraviev-aa@yandex.ru>
2023-03-10 07:27:02 +0300

    Added changes.

--- src/main/java/ru/job4j/formatting/Round.java
+++ src/main/java/ru/job4j/formatting/Round.java
@@ -2,7 +2,8 @@ package ru.job4j.formatting;
 
 public class Round {
     /** Округляем число в большую сторону,
-     * если его дробная часть превышает этот порог */
+     * если его дробная часть превышает этот порог
+     */
     public static final double THRESHOLD = 0.54;
 
     static long round(double d) {
@@ -14,19 +15,44 @@ public class Round {
         }
     }
 
+    /**
+     * Вариант округления
+     */
+    static double roundOne(double d) {
+        double scale = Math.pow(10, 1);
+        return Math.ceil(d * scale) / scale;
+    }
+
+    static double roundTwo(double d) {
+        double scale = Math.pow(10, 2);
+        return Math.ceil(d * scale) / scale;
+    }
+
+    static double roundEight(double d) {
+        double scale = Math.pow(10, 8);
+        return Math.ceil(d * scale) / scale;
+    }
+
     public static void main(String[] args) {
         double a = 3.999999;
         /** Округление до числа с плавающей точкой до ближайшего
-         * целого с помощью метода round() */
+         * целого с помощью метода round()
+         */
         System.out.println(round(a));
 
         int b = (int) a;
-        /** Округление без использования метода round() */
+        /**
+         * Округление без использования метода round()
+         */
         System.out.println(b);
-
         double c = 3.55;
         System.out.println(round(c));
         double d = 3.54;
         System.out.println(round(d));
+        System.out.println("---");
+        double e = 5.123456789;
+        System.out.println(roundOne(e));
+        System.out.println(roundTwo(e));
+        System.out.println(roundEight(e));
     }
 }
