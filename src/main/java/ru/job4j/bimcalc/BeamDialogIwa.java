f1280bc07ac36cc4f0a7f77d85e9afb9dc14ffb1
Aleksei Muraviev <muraviev-aa@yandex.ru>
2023-03-20 14:46:15 +0300

    Added changes.

--- src/main/java/ru/job4j/bimcalc/BeamDialogIwa.java
+++ src/main/java/ru/job4j/bimcalc/BeamDialogIwa.java
@@ -46,6 +46,11 @@ public class BeamDialogIwa implements Bimoment, SectionI {
      */
     private double ay;
     /**
+     * Координата центра тяжести сечени [см]
+     * по оси OY от середины верхней полки
+     */
+    private double aycg;
+    /**
      * Главная секториальная координата w1 [см2]
      */
     private double w1;
@@ -53,6 +58,18 @@ public class BeamDialogIwa implements Bimoment, SectionI {
      * Главная секториальная координата w2 [см2]
      */
     private double w2;
+    /**
+     * Момент инерции [см4]
+     */
+    private double ix;
+    /**
+     * Момент сопротивления сечения [см3]
+     */
+    private double wx1;
+    /**
+     * Момент сопротивления сечения [см3]
+     */
+    private double wx2;
     /**
      * Секториальный момент инерции [см6]
      */
@@ -111,6 +128,49 @@ public class BeamDialogIwa implements Bimoment, SectionI {
                 + ((h - tf1 - tf2) * Math.pow(tw, 3))));
     }
 
+    double calcCenterOfGravity(double tf1, double tf2, double tw, double b, double d, double h) {
+        double h2 = h - 0.5 * (tf1 + tf2);
+        double h3 = 0.5 * (h - tf1 - tf2) + 0.5 * tf1;
+        double s1 = tf1 * b;
+        double s2 = tf2 * d;
+        double s3 = tw * (h - tf1 - tf2);
+        aycg = roundTwo((h2 * s2 + h3 * s3) / (s1 + s2 + s3));
+        return aycg;
+    }
+
+    double calcMomentOfResistance1(double tf1, double tf2, double tw, double h, double b, double d, double aycg) {
+        double s1 = tf1 * b;
+        double s2 = tf2 * d;
+        double s3 = tw * (h - tf1 - tf2);
+        double l = 0.5 * (h - tf1 - tf2) - aycg + 0.5 * tf1;
+        double ix1 = ((b * Math.pow(tf1, 3)) / 12)
+                + s1 * Math.pow(aycg, 2);
+        double ix2 = ((d * Math.pow(tf2, 3)) / 12)
+                + s2 * Math.pow(h - 0.5 * (tf1 + tf2) - aycg, 2);
+        double ix3 = ((tw * Math.pow(h - tf1 - tf2, 3)) / 12)
+                + s3 * Math.pow(l, 2);
+        ix = ix1 + ix2 + ix3;
+        System.out.println("Ix: " + ix);
+        wx1 = roundTwo(ix / (aycg + 0.5 * tf1));
+        return wx1;
+    }
+
+    double calcMomentOfResistance2(double tf1, double tf2, double tw, double h, double b, double d, double aycg) {
+        double s1 = tf1 * b;
+        double s2 = tf2 * d;
+        double s3 = tw * (h - tf1 - tf2);
+        double l = 0.5 * (h - tf1 - tf2) - aycg + 0.5 * tf1;
+        double ix1 = ((b * Math.pow(tf1, 3)) / 12)
+                + s1 * Math.pow(aycg, 2);
+        double ix2 = ((d * Math.pow(tf2, 3)) / 12)
+                + s2 * Math.pow(h - 0.5 * (tf1 + tf2) - aycg, 2);
+        double ix3 = ((tw * Math.pow(h - tf1 - tf2, 3)) / 12)
+                + s3 * Math.pow(l, 2);
+        ix = ix1 + ix2 + ix3;
+        wx2 = roundTwo(ix / (h - (aycg + 0.5 * tf1)));
+        return wx2;
+    }
+
     public static void main(String[] args) {
         BeamDialogIwa beamDialogIwa = new BeamDialogIwa();
         System.out.println("ay: "
@@ -127,5 +187,11 @@ public class BeamDialogIwa implements Bimoment, SectionI {
                 + beamDialogIwa.calcBendingTorsionalCharacteristic(92.34, 4817160.21));
         System.out.println("Bmax: "
                 + beamDialogIwa.calcBimomentMax1(2100, 8, 0.00271132, 600));
+        System.out.println("aycg: "
+                + beamDialogIwa.calcCenterOfGravity(1.6, 1.0, 0.8, 40, 25, 66.6));
+        System.out.println("Wx1: "
+                + beamDialogIwa.calcMomentOfResistance1(1.6, 1.0, 0.8, 66.6, 40, 25, 23.6224));
+        System.out.println("Wx2: "
+                + beamDialogIwa.calcMomentOfResistance2(1.6, 1.0, 0.8, 66.6, 40, 25, 23.6224));
     }
 }
